73984dfce89d66584be5f99ae7934f90213cfd21
M. Sherafati <36226207+herman72@users.noreply.github.com>
2025-04-18 23:37:13 +0300

    :hammer: define entity

--- entity/game.go
+++ entity/game.go
@@ -11,11 +11,11 @@ const (
 type Game struct {
 	ID int
 	Board [][]int
-	currentPlayer int
-	startTime time.Time
+	CurrentPlayer PlayerNmber
+	StartTime time.Time
 	GameStatus GameStatus
-	PlayersHuman []User
-	PlayersAI []AIPlayer
+	PlayersHuman User
+	PlayersAI AIPlayer
 	Winner int
 }
 
@@ -27,3 +27,12 @@ type AIPlayer struct {
 	ID int
 	Name string
 }
+
+type PlayerNmber int 
+const (
+	Player1 PlayerNmber = 1
+	Player2 PlayerNmber = 2
+)
+
+
+
