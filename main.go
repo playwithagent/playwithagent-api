5ad4bc684e93e991a8af6badb17cb6d910d3ae5d
M. Sherafati <36226207+herman72@users.noreply.github.com>
2025-04-20 19:37:18 +0300

    :hammer: clean file

--- main.go
+++ main.go
@@ -4,17 +4,18 @@ import (
 	"fmt"
 	"playwithagent-xo/cmd/httpserver"
 	"playwithagent-xo/config"
-)
 
+)
 
 func main(){
 
-
 	cfg := config.Config{
 		HTTPServer: config.HTTPServer{
-			Port: 8080,},
+			Port: 8080,
+		},
 	}
-
+	
+	
 	server := httpserver.NewServer(cfg)
 	fmt.Println("Start Echo server")
 
