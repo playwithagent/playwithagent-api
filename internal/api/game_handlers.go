473a912ed493298e7b112306977cb11afbd4d64a
M. Sherafati <36226207+herman72@users.noreply.github.com>
2025-04-21 18:01:16 +0300

    :hammer: add get game for handeling game

--- internal/api/game_handlers.go
+++ internal/api/game_handlers.go
@@ -2,15 +2,19 @@ package api
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/http"
 	"playwithagent-xo/internal/game"
+	"playwithagent-xo/internal/repository"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
 
 type GameRepository interface {
 	SaveActiveGame(ctx context.Context, g *game.Game) (int, error)
+	GetActiveGame(ctx context.Context, gameID int) (*game.Game, error)
 }
 
 type GameHandler struct {
@@ -35,4 +39,29 @@ func (h *GameHandler) CreateGame(c echo.Context) error {
 	locationURL := fmt.Sprintf("/api/v1/games/%d", gameID)
 	c.Response().Header().Set(echo.HeaderLocation, locationURL)
 	return c.JSON(http.StatusCreated, newGame)
+}
+
+func (h *GameHandler) GetGame(c echo.Context) error {
+	gameID := c.Param("game_id")
+	
+	gameIDInt, err := strconv.Atoi(gameID)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("Invalid Game ID format: %s", gameID))
+	
+	}
+	if gameIDInt <= 0 {
+		return echo.NewHTTPError(http.StatusBadRequest, "Game ID must be a positive integer")
+	}
+	fmt.Printf("Converted game_id to integer: %d\n", gameIDInt)
+	gameSaved, err := h.repo.GetActiveGame(c.Request().Context(), gameIDInt)
+
+	if err != nil {
+		if errors.Is(err, repository.ErrNotFound) {
+			return echo.NewHTTPError(http.StatusNotFound, fmt.Sprintf("Game with ID %d not found", gameIDInt))
+		}
+		c.Logger().Errorf("Failed to retrieve game %d: %v", gameIDInt, err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "Failed to retrieve game information")
+	}
+
+	return c.JSON(http.StatusOK, gameSaved)	
 }
